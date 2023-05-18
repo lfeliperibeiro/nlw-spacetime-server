6005c092cbe5c21b0f18612ffba548b82deca1ed
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-18 13:50:24 -0300

    feat: add jwt and create conditions

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoute } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
 
 app.register(cors, {
   origin: true,
 })
 
+app.register(authRoutes)
 app.register(memoriesRoute)
 
 app
