fac820713dcec5b0400625461f558e5692787420
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-19 13:26:50 -0300

    feat: create data stream

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoute } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(jwt, {
   secret: 'spacetime',
@@ -15,6 +25,7 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(memoriesRoute)
 
